627052f6d40c308ae5c36dd57f7330c4c6fdae3c
keyleu <kleuyckx@gmail.com>
2022-06-08 13:08:48 +0800

    Added some functions

--- experiment.ts
+++ experiment.ts
@@ -1,5 +1,27 @@
+import { IndexedTx, StargateClient } from "@cosmjs/stargate"
+import { MsgSend } from "cosmjs-types/cosmos/bank/v1beta1/tx"
+import { Tx } from "cosmjs-types/cosmos/tx/v1beta1/tx"
+
+const rpc = "https://rpc.sentry-01.theta-testnet.polypore.xyz" 
+
 const runAll = async(): Promise<void> => {
-    console.log("TODO")
+    const client = await StargateClient.connect(rpc)
+    console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
+    console.log(
+        "Alice balances:",
+        await client.getAllBalances("cosmos1rexyhkkz52qc4v3xsrc8hylm04esww30xv7at9"),
+    )
+    const faucetTx: IndexedTx = (await client.getTx(
+        "540C810D5B379FF4AA5AA5CD45764583CC07EFA37484AAA5BB8174E88114D47D"
+    ))!
+    console.log("Faucet Tx:", faucetTx)
+    const decodedTx: Tx = Tx.decode(faucetTx.tx)
+    console.log("DecodedTx:", decodedTx)
+    console.log("Decoded messages:", decodedTx.body!.messages)
+    const sendMessage: MsgSend = MsgSend.decode(decodedTx.body!.messages[0].value)
+    console.log("Sent message:", sendMessage)
+    const faucet: string = sendMessage.fromAddress
+    console.log("Faucet balances:", await client.getAllBalances(faucet))
 }
 
 runAll()
